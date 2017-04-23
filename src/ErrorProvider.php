bfa9046ebb6c780a224e021e8de8c7f0d357732f
houdunwang <2300071698@qq.com>
2017-04-23 19:55:16 +0800

    no message

--- src/ErrorProvider.php
+++ src/ErrorProvider.php
@@ -13,11 +13,10 @@ use houdunwang\framework\build\Provider;
 
 class ErrorProvider extends Provider {
 	//延迟加载
-	public $defer = false;
+	public $defer = true;
 
 	public function boot() {
-		Config::set( 'error.debug', Config::get( 'app.debug' ) );
-		Error::bootstrap();
+
 	}
 
 	public function register() {
